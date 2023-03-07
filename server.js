74055d717d1e5bdabb2645202e16bf6d450ab0ad
saad4zd <saadmalik4zd@gmail.com>
2023-03-08 01:11:51 +0500

    server.js conflict resolve

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+let port = 3000;
+
+let adminRoutes = require('./routes/adminRoutes');
+let userRoutes = require('./routes/userRoutes');
+
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is Listening on Port : ${port}`);
+});
